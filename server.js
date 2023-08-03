b045dbcc3eb28c088d61143be2e925a084f4d546
DixD123 <nixderp123@gmail.com>
2023-08-03 14:44:15 +0200

    Update BScrypt SYNC

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 
 //END_ASYNC
+
+
+//START_SYNC
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 console.log(hash);
 
 var result = bcrypt.compareSync(myPlaintextPassword, hash);
 console.log(result);
-
-//START_SYNC
-
 
 
 //END_SYNC
